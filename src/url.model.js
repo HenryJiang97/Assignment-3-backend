d3aa328f8321416aa85889072ce3d6780e3cea74
Henry Jiang <henryjiang97@gmail.com>
2020-11-20 15:40:39 -0800

    Added find functions to url model

--- src/url.model.js
+++ src/url.model.js
@@ -4,15 +4,29 @@ const UrlSchema = require('./url.schema').UrlSchema;
 
 const UrlModel = mongoose.model("Url", UrlSchema);
 
-function insertUrl(Url) {
-    return UrlModel.create(Url);
+// Insert a new url to the database
+function insertUrl(url) {
+    return UrlModel.create(url);
 }
 
+// Get all urls in the database
 function getAllUrls() {
     return UrlModel.find().exec();
+}
+
+// Find url by long_url value
+function findUrlByLongUrl(long_url) {
+    return UrlModel.find({long_url: long_url}).exec();
+}
+
+// Find url by short_url value
+function findUrlByShortUrl(short_url) {
+    return UrlModel.find({short_url: short_url}).exec();
 }
 
 module.exports = {
     insertUrl, 
     getAllUrls,
+    findUrlByLongUrl,
+    findUrlByShortUrl
 }
